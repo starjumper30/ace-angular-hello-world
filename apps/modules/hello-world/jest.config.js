1e2aa17ed5bda48472cbf9666ad6bcc1b5d788c9
Chris Hardin <chris.hardin@oasisdigital.com>
2021-04-28 12:35:06 -0500

    Add server and client apps

---
+++ apps/modules/hello-world/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'modules-hello-world',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  coverageDirectory: '../../../coverage/apps/modules/hello-world',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/hello-world',
+    '<rootDir>/apps/modules/hello-world',
+  ],
+};
